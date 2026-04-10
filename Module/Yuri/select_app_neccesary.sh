50a6fce8a1aef63a81f6968f8c05bcc8eff16304
Tam97123 <219667310+Tam97123@users.noreply.github.com>
2026-04-10 19:08:39 +0700

    refactor(target): Stop force using Leaf hack mode

--- Module/Yuri/select_app_neccesary.sh
+++ Module/Yuri/select_app_neccesary.sh
@@ -15,26 +15,30 @@ rm -rf "$t"
 # add list special
 fixed_targets="\
 android
-com.android.vending!
-com.google.android.gsf!
-com.google.android.gms!
-io.github.vvb2060.keyattestation!
-io.github.vvb2060.mahoshojo!
-com.google.android.contactkeys!
-com.google.android.ims!
-com.google.android.safetycore!
-com.google.android.apps.nbu.paisa.user!
-com.youhu.laifu!
-com.whatsapp!
-com.whatsapp.w4b!
-com.reveny.nativecheck!
-icu.nullptr.nativetest!
-com.android.nativetest!
-io.liankong.riskdetector!
-me.garfieldhan.holmes!
-luna.safe.luna!
-com.zhenxi.hunter!
-com.studio.duckdetector?"
+com.android.vending
+com.google.android.gsf
+com.google.android.gms
+com.google.android.ims
+io.github.vvb2060.keyattestation?
+io.github.vvb2060.mahoshojo?
+io.github.qwq233.keyattestation?
+com.google.android.contactkeys
+com.google.android.safetycore
+com.google.android.apps.walletnfcrel
+com.google.android.apps.nbu.paisa.user
+com.reveny.nativecheck?
+icu.nullptr.nativetest?
+com.android.nativetest?
+io.liankong.riskdetector?
+me.garfieldhan.holmes?
+luna.safe.luna?
+com.zhenxi.hunter?
+com.studio.duckdetector?
+com.eltavine.duckdetector?
+com.rem01gaming.disclosure?
+wu.keyChain.test?
+com.kikyps.crackme?
+com.chunqiunativecheck?"
 for entry in $fixed_targets; do
     if ! echo "$entry" >> "$t"; then
         log_message "ERROR: Failed to write $entry to $t"
