c43d1206cc14fd566f00b919d933e63358bf0056
Tam97123 <219667310+Tam97123@users.noreply.github.com>
2026-04-24 18:37:10 +0700

    fix(action): Revert to old action

--- Module/action.sh
+++ Module/action.sh
@@ -1,7 +1,18 @@
 MODPATH="${0%/*}"
 
-cd "$MODPATH/Yuri/action"
-sh integrity.sh
+for SCRIPT in \
+  "kill_google_process.sh" \
+  "target_txt.sh" \
+  "security_patch.sh" \
+  "boot_hash.sh" \
+  "yuri_keybox.sh"
+do
+  if ! sh "$MODPATH/Yuri/$SCRIPT"; then
+    echo "- Error: $SCRIPT failed. Aborting..."
+    exit 1
+  fi
+done
+  sh "$MODPATH/Yuri/pif.sh"
 
 if [ -f /data/adb/modules_update/Yurikey/webroot/common/device-info.sh ]; then
   sh /data/adb/modules_update/Yurikey/webroot/common/device-info.sh
